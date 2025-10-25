675924ec78d4c352dd52eed15e7035445bb2e934
mukles <muklesdev@gmail.com>
2025-10-25 12:12:07 +0600

    feat(database): configure TypeOrmModule with async loading and environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -7,6 +9,16 @@ import { AppService } from './app.service';
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
+        entities: [path.join(process.cwd(), 'dist/**/*.entity{.ts,.js}')],
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [AppController],
