a86093336cf46ebdb89bc6513feebd6a6642b31e
mukles <muklesdev@gmail.com>
2025-10-25 22:47:46 +0600

    feat: implement CreateTeacherDto with validation rules

--- src/teacher/dto/create-teacher.dto.ts
+++ src/teacher/dto/create-teacher.dto.ts
@@ -1,1 +1,40 @@
-export class CreateTeacherDto {}
+import {
+  IsDateString,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateTeacherDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  readonly firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  readonly lastName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @MaxLength(100)
+  readonly email: string;
+
+  @IsDateString()
+  @IsNotEmpty()
+  readonly dateOfBirth: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  @MaxLength(100)
+  readonly password: string;
+
+  @IsEnum(['male', 'female', 'others'])
+  @IsNotEmpty()
+  readonly gender: 'male' | 'female' | 'others';
+}
