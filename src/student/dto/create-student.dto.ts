9883c26613dc3ad8ce5e16bbb5bd762c1eb474f3
mukles <muklesdev@gmail.com>
2025-12-19 23:58:53 +0600

    feat: enhance CreateStudentDto with validation and Swagger properties

--- src/student/dto/create-student.dto.ts
+++ src/student/dto/create-student.dto.ts
@@ -1,15 +1,30 @@
-import { IsDate, IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Gender, StudentStatus } from '../entities/student.entity';
+import { IsDate, IsEmail, IsNotEmpty, IsEnum, IsOptional } from 'class-validator';
 
 export class CreateStudentDto {
+  @ApiProperty()
   @IsNotEmpty()
   firstName: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   lastName: string;
 
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({ type: String, format: 'date-time' })
   @IsDate()
   dateOfBirth: Date;
+
+  @ApiProperty({ enum: Gender })
+  @IsEnum(Gender)
+  gender: Gender;
+
+  @ApiProperty({ enum: StudentStatus, required: false })
+  @IsOptional()
+  @IsEnum(StudentStatus)
+  status?: StudentStatus;
 }
--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,23 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 
 import { StudentService } from './student.service';
 
+import { CreateStudentDto } from './dto/create-student.dto';
+import { StudentResponseDto } from './dto/student-response.dto';
+
+@ApiTags('student')
 @Controller('student')
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
 
   @Get()
-  async findAll() {
-    return await this.studentService.findAllStudents();
+  async findAll(): Promise<StudentResponseDto[]> {
+    const students = await this.studentService.findAllStudents();
+    return students;
   }
 
-  @Get('create')
-  async create() {
-    return await this.studentService.createStudent();
+  @Post('create')
+  @ApiBody({ type: CreateStudentDto, description: 'Student data' })
+  async create(@Body() student: CreateStudentDto) {
+    return await this.studentService.createStudent(student);
   }
 
   @Get('create-multiple')
-  async createMultiple() {
-    return await this.studentService.createMultipleStudents();
-  }
+  async createMultiple() {}
 }
