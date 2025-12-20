0fa11f46dc51dbb3770156564eebf1008a4e8141
mukles <muklesdev@gmail.com>
2025-12-20 21:47:22 +0600

    feat: add User module and service for user management

---
+++ src/user/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+
+import { Role } from '../role/entities/role.entity';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(private userRepository: Repository<User>) {}
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.userRepository.findOne({ where: { email } });
+  }
+
+  async findById(id: number): Promise<User | null> {
+    return this.userRepository.findOne({ where: { id } });
+  }
+
+  async create(userData: Partial<User>, role: Role): Promise<User> {
+    const user = this.userRepository.create({ ...userData, role });
+    return this.userRepository.save(user);
+  }
+
+  async updateRefreshToken(userId: number, refreshToken: string | null): Promise<void> {
+    await this.userRepository.update(userId, { refreshToken });
+  }
+}
