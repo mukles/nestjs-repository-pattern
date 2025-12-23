db2e647818a372610a467c73a232171918cb5330
mukles <muklesdev@gmail.com>
2025-12-23 23:37:52 +0600

    feat: implement HttpExceptionFilter for centralized error handling and update ApiErrorResponse structure

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
