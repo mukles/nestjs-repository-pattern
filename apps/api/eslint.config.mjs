fbae8c71db9a4939d72a6b792f7343c6339daef9
mukles <muklesdev@gmail.com>
2026-01-28 14:02:08 +0600

    refactor: Consolidate ESLint ignore patterns into `eslint.config.mjs`, expand `tsconfig.json` includes, and refine `Button` component styling.

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -1,4 +1,9 @@
 import { nestJsConfig } from '@repo/eslint-config/nest-js';
 
-/** @type {import("eslint").Linter.Config} */
-export default nestJsConfig;
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**', 'src/migrations/**', 'test/**'],
+  },
+  ...nestJsConfig,
+];
--- packages/ui/src/components/ui-kit/button.tsx
+++ packages/ui/src/components/ui-kit/button.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 import { cn } from 'src/lib/utils';
 
 const buttonVariants = cva(
-  "absolute inline-flex shrink-0 items-center justify-center gap-2 rounded-md text-sm font-medium whitespace-nowrap transition-all outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+  "inline-flex shrink-0 items-center justify-center gap-2 rounded-md text-sm font-medium whitespace-nowrap transition-all outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
   {
     variants: {
       variant: {
@@ -57,7 +57,9 @@ function Button({
       data-size={size}
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
-    />
+    >
+      <div className="m-2 bg-red-500 p-4 text-2xl" />
+    </Comp>
   );
 }
 
