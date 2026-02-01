9b50f393fcff28e42b4506b3f101829259b0a7ad
mukles <muklesdev@gmail.com>
2026-02-01 12:24:55 +0600

    feat: Refactor authentication actions and improve error handling; update environment variables for JWT configuration

--- apps/admin/src/actions/auth/index.ts
+++ apps/admin/src/actions/auth/index.ts
@@ -1,34 +1,38 @@
-"use server";
-import "server-only";
+'use server';
+import 'server-only';
 
-import { createSession, deleteSession } from "@/lib/session";
-import { loginUserSchema } from "@/lib/validation/user.schema";
-import { redirect } from "next/navigation";
-import { AuthResponse } from "./types";
-import { apiAction, Result, safeAction } from "../common";
+import { createSession, deleteSession } from '@/lib/session';
+import { loginUserSchema } from '@/lib/validation/user.schema';
+import { redirect } from 'next/navigation';
+import { apiAction, Result, safeAction } from '../common';
+import { AuthResponse } from './types';
 
 export const loginUser = async (
   _state: Result<AuthResponse>,
   formData: FormData,
 ) => {
   return safeAction(async () => {
-    const data = Object.fromEntries(formData);
-    const validatedData = loginUserSchema.parse(data);
-    const response = await apiAction<AuthResponse>("/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(validatedData),
-    });
+    try {
+      const data = Object.fromEntries(formData);
+      const validatedData = loginUserSchema.parse(data);
+      const response = await apiAction<AuthResponse>('/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(validatedData),
+      });
 
-    await createSession(response.accessToken, response.refreshToken);
+      await createSession(response.accessToken, response.refreshToken);
 
-    return response;
+      return response;
+    } catch (error) {
+      console.error('Login Error:', error);
+    }
   });
 };
 
 export async function logout() {
   await deleteSession();
-  redirect("/login");
+  redirect('/login');
 }
